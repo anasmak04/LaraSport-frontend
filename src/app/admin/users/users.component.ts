e2d540de0c1aa96e5917064e665116f28c452b31
anasmak04 <anasdev8@gmail.com>
2024-04-03 11:04:32 +0000

    added search in admin dashboard

--- src/app/admin/users/users.component.ts
+++ src/app/admin/users/users.component.ts
@@ -1,35 +1,66 @@
-import { Component, OnInit, inject } from '@angular/core';
-import { LoaderServiceService } from 'src/app/services/loader/loader-service.service';
-import { UsersService } from 'src/app/services/users/users.service';
+import { Component, OnInit, inject } from "@angular/core";
+import { LoaderServiceService } from "src/app/services/loader/loader-service.service";
+import { UsersService } from "src/app/services/users/users.service";
 
 @Component({
-  selector: 'app-users',
-  templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  selector: "app-users",
+  templateUrl: "./users.component.html",
+  styleUrls: ["./users.component.css"],
 })
-export class UsersComponent implements OnInit{
+export class UsersComponent implements OnInit {
+  users: any = [];
+  Search: any = [];
+  searchTerm: String = "";
+  selectedItemId: number | null = null;
+  selectedRoleId: number | null = null;
 
-  users :any = [];
-  constructor(private userservice : UsersService){
-
-  }
+  constructor(private userservice: UsersService) {}
 
   loader = inject(LoaderServiceService);
 
-  ngOnInit(): void {  
+  ngOnInit(): void {
     this.findAll();
   }
 
-
-  findAll(){
+  findAll() {
     this.userservice.findAllUsers().subscribe({
-      next : (response) => {          
-        this.users = response.user
-        console.log(this.users)
+      next: (response) => {
+        this.users = response.user;
+        console.log(this.users);
       },
-
-      error : (err) => console.log(err)
-    })
+      error: (err) => console.log(err),
+    });
   }
 
+  toggleModal(itemId: number | null): void {
+    this.selectedItemId = itemId;
+    if (itemId !== null) this.selectedRoleId = null;
+  }
+
+  attachRole(userId: number, roleId: number) {
+    if (!userId || !roleId) {
+      console.error("User ID or Role ID is missing");
+      return;
+    }
+
+    this.userservice.updateRole(userId, roleId).subscribe({
+      next: (response) => {
+        console.log("Role attached successfully:", response);
+      },
+      error: (err) => {
+        console.error("Error attaching role:", err);
+      },
+    });
+  }
+
+  search(searchTerm : String) {
+    return this.userservice.search(searchTerm).subscribe({
+      next: (response) => {
+        this.Search = response.user;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 }
--- src/app/services/users/users.service.ts
+++ src/app/services/users/users.service.ts
@@ -8,6 +8,8 @@ import { UserResponse } from "src/app/shared/model/user/user-response";
 })
 export class UsersService {
   Api = "http://127.0.0.1:8000/api/user";
+  APIURL = "http://127.0.0.1:8000/api/user/";
+  ApiSearch = "http://127.0.0.1:8000/api/search/user/";
 
   constructor(private http: HttpClient) {}
 
@@ -15,5 +17,13 @@ export class UsersService {
     return this.http.get<UserResponse>(this.Api);
   }
 
-  
+  updateRole(userId: number, roleId: number): Observable<any> {
+    const url = `${this.Api}${userId}/role`;
+    const body = { role_id: roleId };
+    return this.http.put<any>(url, body);
+  }
+
+  search(sarchTerm: String): Observable<any> {
+    return this.http.get<any>(this.ApiSearch + sarchTerm);
+  }
 }
